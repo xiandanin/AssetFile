563d15f3e921143d43a5b5c3ebef3cf50204b2ac
dengyuhan <464958567@qq.com>
2018-06-27 21:01:51 +0800

    例子

--- example/src/main/java/com/dyhdyh/manager/assets/example/MainActivity.java
+++ example/src/main/java/com/dyhdyh/manager/assets/example/MainActivity.java
@@ -2,18 +2,17 @@ package com.dyhdyh.manager.assets.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.dyhdyh.manager.assets.AssetsManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AssetsManager(this)
-                .copyAssetDir("test", getExternalCacheDir());
+        rv = findViewById(R.id.rv);
 
     }
 }
